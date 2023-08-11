c52616bbe911b95f4e89922bbb0d5b058cf2453d
Anuraag-CH <anuraag.ch@gmail.com>
2023-08-12 01:15:42 +0530

    initial commit

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,8 +1,7 @@
 import { z } from "zod";
 export const signupInput = z.object({
-    username: z.string(),
-    password: z.string()
-})
+  username: z.string(),
+  password: z.string(),
+});
 
 export type SignupParams = z.infer<typeof signupInput>;
-  
